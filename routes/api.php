8a06b6248b7753f8cb1ebdd682b0fabb4a6f1b14
Alan <removilak3000@gmail.com>
2026-03-31 16:19:14 -0600

    REGISTRO USUARIOS

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,7 @@ use Illuminate\Http\Request;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [UserController::class, 'store']);
 Route::get('catalog', [App\Http\Controllers\User\CatalogController::class, 'index']);
 Route::get('catalog/{id}', [App\Http\Controllers\User\CatalogController::class, 'show']);
 
